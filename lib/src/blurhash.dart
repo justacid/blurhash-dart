a104b49f3c87e6d1a138643664992f0d449bb364
Tobias Schwackenhofer <tobiasschw@gmail.com>
2020-02-22 17:36:48 +0100

    Implement encoding of blurhashes

--- lib/src/blurhash.dart
+++ lib/src/blurhash.dart
@@ -59,6 +59,74 @@ Uint8List decodeBlurHash(
   return buildBitmap(bytes, width, height);
 }
 
+/// Encodes an image to a BlurHash string
+///
+/// The format of the given [data] array is epected to be raw pixels in RGBA32 format -
+/// without any additional image headers. The [width] and [height] are the dimensions of
+/// the given image. Paramters [numCompX] and [numCompY] are the components of the
+/// BlurHash. Both parameters must lie between 1 and 9.
+///
+/// Throws [BlurHashEncodeException] when [numCompX] and [numCompY] do not lie within the
+/// expected range. Also throws [BlurHashDecodeException] when the [data] array is not in
+/// the expeted RGBA32 format.
+String encodeBlurHash(
+  Uint8List data,
+  int width,
+  int height, {
+  int numCompX = 4,
+  int numpCompY = 3,
+}) {
+  if (numCompX < 1 || numCompX > 9 || numpCompY < 1 || numCompX > 9) {
+    throw BlurHashEncodeException(
+      message: "BlurHash components must lie between 1 and 9.",
+    );
+  }
+
+  if (width * height * 4 != data.length) {
+    throw BlurHashEncodeException(
+      message: "The width and height must match the data array."
+          "The expected format is RGBA32",
+    );
+  }
+
+  final factors = List<Color>(numCompX * numpCompY);
+  int i = 0;
+  for (var y = 0; y < numpCompY; ++y) {
+    for (var x = 0; x < numCompX; ++x) {
+      final normalisation = (x == 0 && y == 0) ? 1.0 : 2.0;
+      final basisFunc = (int i, int j) {
+        return normalisation *
+            cos((pi * x * i) / width) *
+            cos((pi * y * j) / height);
+      };
+      factors[i++] = _multiplyBasisFunction(data, width, height, basisFunc);
+    }
+  }
+
+  final dc = factors.first;
+  final ac = factors.skip(1).toList();
+
+  final blurHash = StringBuffer();
+  final sizeFlag = (numCompX - 1) + (numpCompY - 1) * 9;
+  blurHash.write(encode83(sizeFlag, 1));
+
+  var maxVal = 1.0;
+  if (ac.length > 0) {
+    final maxElem = (Color c) => max(c.r.abs(), max(c.g.abs(), c.b.abs()));
+    final actualMax = ac.map(maxElem).reduce(max);
+    final quantisedMax = max(0, min(82, (actualMax * 166.0 - 0.5).floor()));
+    maxVal = (quantisedMax + 1.0) / 166.0;
+    blurHash.write(encode83(quantisedMax, 1));
+  } else {
+    blurHash.write(encode83(0, 1));
+  }
+
+  blurHash.write(encode83(encodeDC(dc), 4));
+  for (final factor in ac)
+    blurHash.write(encode83(encodeAC(factor, maxVal), 2));
+  return blurHash.toString();
+}
+
 Uint8List _transform(
   int width,
   int height,
@@ -93,4 +161,29 @@ Uint8List _transform(
   }
 
   return Uint8List.fromList(pixels);
+}
+
+Color _multiplyBasisFunction(
+  Uint8List pixels,
+  int width,
+  int height,
+  double basisFunction(int i, int j),
+) {
+  var r = 0.0;
+  var g = 0.0;
+  var b = 0.0;
+
+  final bytesPerRow = width * 4;
+
+  for (var x = 0; x < width; ++x) {
+    for (var y = 0; y < height; ++y) {
+      final basis = basisFunction(x, y);
+      r += basis * sRGBtoLinear(pixels[4 * x + 0 + y * bytesPerRow]);
+      g += basis * sRGBtoLinear(pixels[4 * x + 1 + y * bytesPerRow]);
+      b += basis * sRGBtoLinear(pixels[4 * x + 2 + y * bytesPerRow]);
+    }
+  }
+
+  final scale = 1.0 / (width * height);
+  return Color(r * scale, g * scale, b * scale);
 }
--- lib/src/exception.dart
+++ lib/src/exception.dart
@@ -2,3 +2,8 @@ class BlurHashDecodeException implements Exception {
   BlurHashDecodeException({String message}) : message = message ?? "";
   final String message;
 }
+
+class BlurHashEncodeException implements Exception {
+  BlurHashEncodeException({String message}) : message = message ?? "";
+  final String message;
+}
