4eded3bf2a103cfcf9e2500f1282931823411fe5
Robin_Alter <robin-alter@hotmail.de>
2020-10-20 12:24:37 +0200

    Updated the example to match, new functionality

--- example/example.dart
+++ example/example.dart
@@ -3,7 +3,8 @@ import 'dart:typed_data';
 import 'package:blurhash_dart/blurhash_dart.dart';
 
 void main() {
-  String blurHash = "LEHV6nWB2yk8pyo0adR*.7kCMdnj";
-  Uint8List pixels = decodeBlurHash(blurHash, 35, 20);
-  print("Do something with the $pixels...");
+  String hash = "LEHV6nWB2yk8pyo0adR*.7kCMdnj";
+  BlurHash blurHash = decodeBlurHash(hash, 35, 20);
+  Uint8List bitmap = blurHash.toBitmap();
+  print("Do something with the $bitmap...");
 }
